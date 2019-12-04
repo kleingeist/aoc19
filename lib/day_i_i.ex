62b8591105bdc693bbb7dbafb0ae92e2beeed6e1
jo <kleingeist@googlemail.com>
2019-12-04 22:17:52 +0100

    Day 2 Task 2

--- lib/day_i_i.ex
+++ lib/day_i_i.ex
@@ -9,4 +9,20 @@ defmodule DayII do
     IntCodeInterpreter.run(input_list, 12, 2)
   end
 
+  def task2(input_file) do
+    input_list = File.read!(input_file)
+                 |> String.split(",")
+                 |> Enum.map(&String.trim/1)
+                 |> Enum.map(&String.to_integer/1)
+
+    expected_output = 19690720
+
+    inputs = for noun <- 0..99, verb <- 0..99, do: {noun, verb}
+    {noun, verb} = Enum.find(
+      inputs,
+      nil,
+      fn {noun, verb} -> IntCodeInterpreter.run(input_list, noun, verb) == expected_output end
+    )
+    100 * noun + verb
+  end
 end
