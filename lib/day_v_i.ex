4e09cc90682e8394c71a83563e72cba5d737c9c5
jo <kleingeist@googlemail.com>
2019-12-27 17:35:41 +0100

    Day VI task 2

--- lib/day_v_i.ex
+++ lib/day_v_i.ex
@@ -8,6 +8,57 @@ defmodule DayVI do
     checksum
   end
 
+  def task2b(input_file) do
+    g = read_input_file(input_file) |> create_graph()
+    {root, _} = :digraph.vertex(g, "COM")
+    {you, _} = :digraph.vertex(g, "YOU")
+    {san, _} = :digraph.vertex(g, "SAN")
+
+    path_you = :digraph.get_path(g, root, you)
+    path_san = :digraph.get_path(g, root, san)
+
+    :digraph.delete(g)
+
+    prefix = common_prefix(path_you, path_san)
+    prefix_length = Enum.count(prefix)
+    you_from_common = Enum.count(path_you) - prefix_length - 1
+    san_from_common = Enum.count(path_san) - prefix_length - 1
+    you_from_common + san_from_common
+  end
+
+  def common_prefix(a, b) do
+    Enum.zip(a, b)
+    |> Enum.take_while(fn {a, b} -> a == b end)
+  end
+
+  def task2(input_file) do
+    g = read_input_file(input_file) |> create_graph()
+    {root, _} = :digraph.vertex(g, "COM")
+    {you, _} = :digraph.vertex(g, "YOU")
+    {san, _} = :digraph.vertex(g, "SAN")
+
+    :ok = add_back_edges(g, you, root)
+    path = :digraph.get_short_path(g, you, san)
+    :digraph.delete(g)
+    Enum.count(path) - 2 - 1
+  end
+
+  def add_back_edges(g, current, root) do
+    [parent] = :digraph.in_neighbours(g, current)
+    add_back_edges(g, parent, root, current)
+  end
+
+  def add_back_edges(g, root, root, child) do
+    :digraph.add_edge(g, child, root)
+    :ok
+  end
+
+  def add_back_edges(g, current, root, child) do
+    [parent] = :digraph.in_neighbours(g, current)
+    :digraph.add_edge(g, child, current)
+    add_back_edges(g, parent, root, current)
+  end
+
   def orbit_checksum(g) do
     {root, _} = :digraph.vertex(g, "COM")
     orbit_checksum(g, [root], 0)
@@ -23,12 +74,15 @@ defmodule DayVI do
   end
 
   def create_graph(input_list) do
-    g = :digraph.new([:acyclic])
+    # g = :digraph.new([:acyclic])
+    g = :digraph.new()
+
     for [vv1, vv2] <- input_list do
       v1 = :digraph.add_vertex(g, vv1)
       v2 = :digraph.add_vertex(g, vv2)
       :digraph.add_edge(g, v1, v2)
     end
+
     g
   end
 
