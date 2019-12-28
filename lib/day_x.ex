486811b0662407cd69d45229fbfbfafb976cab94
jo <kleingeist@googlemail.com>
2019-12-28 20:45:01 +0100

    Day X - task2 (upperleft coords are stupid)

--- lib/day_x.ex
+++ lib/day_x.ex
@@ -22,6 +22,15 @@ defmodule DayX do
     |> parse_asteroids()
     |> sights()
     |> Enum.max_by(&elem(&1, 1))
+    |> elem(0)
+  end
+
+  def task1b(input_file) do
+    asteroids =
+      read_asteroids(input_file)
+      |> parse_asteroids()
+
+      Enum.max_by(asteroids, fn station -> run_radar(station, asteroids) |> Enum.count() end)
   end
 
   def sights(asteroids) do
@@ -34,17 +43,60 @@ defmodule DayX do
     end)
   end
 
-  # def slopes(list) do
-  #   pairs(list)
-  #   |> Enum.map(&get_slope/1)
-  #   |> Enum.uniq()
-  # end
+  def task2(input_file) do
+    asteroids =
+      read_asteroids(input_file)
+      |> parse_asteroids()
 
-  # def pairs([x | rest]) do
-  #   Enum.map(rest, &{x, &1}) ++ pairs(rest)
-  # end
+    # known form task1
+    # x = {20, 19}
+    station = task1(input_file)
 
-  # def pairs([]), do: []
+    radar = run_radar(station, asteroids)
+    vaporder = turnit(radar, [], [])
+    IO.inspect(vaporder, limit: :infinity)
+    # {Enum.at(vaporder, 199), Enum.at(vaporder, 200)}
+    # vaporder
+    Enum.at(vaporder, 199)
+  end
+
+  @spec run_radar(any, any) :: [any]
+  def run_radar(station, asteroids) do
+    Enum.reject(asteroids, &(&1 == station))
+    |> Enum.map(fn asteroid ->
+      a = {station, asteroid}
+      {asteroid, get_slope(a), get_distance(a)}
+    end)
+    |> Enum.group_by(&elem(&1, 1))
+    |> Enum.map(fn {key, value} -> {key, Enum.sort_by(value, &elem(&1, 2))} end)
+    |> Enum.sort_by(fn {slope, _} -> get_angle(slope) end)
+    |> Enum.map(fn {_, value} -> value end)
+  end
+
+  def turnit([[asteroid | line] | radar], next, acc) do
+    turnit(radar, [line | next], [asteroid | acc])
+  end
+
+  def turnit([], [], acc) do
+    Enum.reverse(acc)
+  end
+
+  def turnit([], next, acc) do
+    IO.inspect(Enum.count(acc))
+    radar = Enum.reverse(next) |> Enum.reject(&(&1 == []))
+    turnit(radar, [], acc)
+  end
+
+  def slopes(list) do
+    pairs(list)
+    |> Enum.map(&get_slope/1)
+  end
+
+  def pairs([x | rest]) do
+    Enum.map(rest, &{x, &1}) ++ pairs(rest)
+  end
+
+  def pairs([]), do: []
 
   def get_slope({{x1, y1}, {x2, y2}}) do
     s = {x2 - x1, y2 - y1}
@@ -52,10 +104,22 @@ defmodule DayX do
   end
 
   def norm_slope({x, y}) do
-    d = gcd(x, y)
+    d = Math.gcd(x, y)
     {div(x, d), div(y, d)}
   end
 
-  def gcd(a, 0), do: abs(a)
-  def gcd(a, b), do: gcd(b, rem(a, b))
+  def get_distance({{x1, y1}, {x2, y2}}) do
+    abs(x2 - x1) + abs(y2 - y1)
+  end
+
+  def get_angle({x, y}) do
+    cond do
+      x == 0 && y >= 0 -> 0
+      x == 0 && y < 0 -> :math.pi()
+      x >= 0 && y <= 0 -> :math.pi() / 2 - :math.atan(y / x)
+      x >= 0 && y >= 0 -> :math.pi() + :math.atan(y / x)
+      x <= 0 && y >= 0 -> :math.pi() + :math.atan(y / x)
+      x <= 0 && y <= 0 -> :math.pi() * 2 + :math.atan(y / x)
+    end
+  end
 end
